2649a7c89d6569a5af20b3427bf800fe57e59300
Arpit Yadav <arpityadavay2005@gmail.com>
2024-11-28 15:01:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  const date = dateParam 
+    ? isNaN(dateParam)
+      ? new Date(dateParam)
+      : new Date(parseInt(dateParam))
+    : new Date();
+
+    if(isNaN(date.getTime())) {
+      return res.json({error: "Invalid Date"});
+    }
+
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
 });
 
 
